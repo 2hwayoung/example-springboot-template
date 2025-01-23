8eecba8a77f889660c372cef85f72d1a34779f84
2hwayoung <hwa3270@gmail.com>
2025-01-23 15:24:12 +0900

    feat: add PostController with write api

--- src/main/java/com/springboot/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/springboot/template/domain/post/post/controller/PostController.java
@@ -1,16 +1,43 @@
 package com.springboot.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping("/")
+    @GetMapping("/write")
     @ResponseBody
-    public String hello() {
-        return "hello";
+    public String showWrite() {
+        return getFormHtml("");
+    }
+
+    @PostMapping("/write")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+        if (title.isBlank() || title.isEmpty()) return getFormHtml("제목을 입력해주세요.");
+        if (content.isBlank() || content.isEmpty()) return getFormHtml("내용을 입력해주세요");
+
+        if (title.length() < 5) return getFormHtml("제목은 5글자 이상 작성해주세요.");
+        if (content.length() < 10) return getFormHtml("내용은 10글자 이상 작성해주세요.");
+
+        return """
+                <h1>게시물 조회</h1>
+                <div>%s</div>
+                <div>%s</div>
+                """.formatted(title, content);
+    }
+
+    public String getFormHtml(String errorMsg) {
+        return """
+                <div>%s</div>
+                <form method="post">
+                    <input type="text" name="title" placeholder="제목" /> <br>
+                    <textarea name="content"></textarea> <br>
+                    <input type="submit" value="등록" /> <br>
+                </form>
+                """.formatted(errorMsg);
     }
 
 }
